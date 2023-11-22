ec5fa4891dafb3740cb01e5a2bb2ac53f2436460
nimosarang <111734043+nimosarang@users.noreply.github.com>
2023-11-22 10:38:41 +0900

    파일 이름 파라미터 전달 그리고 검증

---
+++ src/main/java/com/SpringBatchTest/job/validator/FileParamValidator.java
@@ -0,0 +1,24 @@
+package com.SpringBatchTest.job.validator;
+
+import org.springframework.batch.core.JobParameters;
+import org.springframework.batch.core.JobParametersInvalidException;
+import org.springframework.batch.core.JobParametersValidator;
+import org.springframework.util.StringUtils;
+
+public class FileParamValidator implements JobParametersValidator {
+
+    @Override
+    public void validate(JobParameters parameters) throws JobParametersInvalidException {
+
+        String fileName = null;
+
+        if (parameters != null) {
+            fileName = parameters.getString("fileName");
+        }
+
+        if (!StringUtils.endsWithIgnoreCase(fileName, "csv")) {
+            throw new JobParametersInvalidException("This is not csv file");
+        }
+
+    }
+}
